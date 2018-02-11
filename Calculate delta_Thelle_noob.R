935b9281ccbb855b0b3da0317f70192aa3e9564f
Thelle Christensen <thelle.k@gmail.com>
2018-02-11 17:14:22 +0100

    Adding Delta calculation with QuantMod

--- Calculate delta_Thelle_noob.R
+++ Calculate delta_Thelle_noob.R
@@ -1,3 +1,9 @@
+# Get quantmod
+if (!require("quantmod")) {
+  install.packages("quantmod")
+  library(quantmod)
+}
+
 # Read CSV into R
 MyData <- read.csv(file="/Users/thelle/Google Drive/Projects/Stock Analysis_Dual share class/twinstock_github/data/allTwins.csv", header=TRUE, sep=",")
 
@@ -31,7 +37,7 @@ for(i in seq(from=6, to=255, by=12)){
   print((as.numeric(latestValues[i])-as.numeric(latestValues[i+6]))/as.numeric(latestValues[i]))
 }
 
-#Using for loop to append latest deltas to a vector
+#Using for loop to append latest deltas to a vector - LOOP NOT WORKING YET - NOOB NOOB
 latestDeltaList = list()
 for (j in seq(from=1, to=24, by=1)){
   for(i in seq(from=6, to=255, by=12)){
@@ -41,3 +47,15 @@ for (j in seq(from=1, to=24, by=1)){
 }
 
 latestDeltaList
+
+
+#Calculating delta in bulk using quantmod package -- pct fucntion in r might also do the job..
+Stock.Open <- c(102.25,102.87,102.25,100.87,103.44,103.87,103.00)
+Stock.Close <- c(102.12,102.62,100.12,103.00,103.87,103.12,105.12)
+
+Delt(Stock.Open)                    #one period pct. price change
+Delt(Stock.Open,k=1)                #same
+Delt(Stock.Open,type='arithmetic')  #using arithmetic differences
+
+Delt(Stock.Open,Stock.Close)        #Open to Close pct. change
+Delt(Stock.Open,Stock.Close,k=0:2)  #...for 0,1, and 2 periods
